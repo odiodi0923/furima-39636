ce6454fea0eb484ec641bc199f18ace15e6d6308
odiodi0923 <141980992+odiodi0923@users.noreply.github.com>
2023-10-16 23:31:14 +0900

    テストコードの作成
    「user_spec.rb」は前回のユーザー管理機能の実装ですが、一部修正しました。

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,22 @@
 FactoryBot.define do
   factory :item do
+
+    Faker::Config.locale = :ja
+    
+    item_name             { Faker::Lorem.sentence(word_count: 15) }
+    description           { Faker::Lorem.paragraph }
+    category_id           { Faker::Number.between(from:1, to: 11) }
+    condition_id          { Faker::Number.between(from:1, to: 7) }
+    fee_option_id         { Faker::Number.between(from:1, to: 3) }
+    region_id             { Faker::Number.between(from:1, to: 48) }
+    delivery_d_id         { Faker::Number.between(from:1, to: 4) }
+    price                 { Faker::Number.between(from:300, to: 9_999_999) }
+
+    association :user
+
+    after(:build) do |message|
+      message.item_image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
     
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
+
   end
 
   describe 'ユーザー新規登録' do
@@ -93,7 +94,7 @@ RSpec.describe User, type: :model do
       end
 
       it 'last_nameが半角では登録できない' do
-        @user.last_name = Faker::Name.last_name
+        @user.last_name = kato
         @user.valid?
         expect(@user.errors.full_messages).to include('Last name  is invalid. Input full-width characters')
       end
@@ -105,7 +106,7 @@ RSpec.describe User, type: :model do
       end
 
       it 'first_nameが半角では登録できない' do
-        @user.first_name = Faker::Name.first_name
+        @user.first_name = taro
         @user.valid?
         expect(@user.errors.full_messages).to include('First name  is invalid. Input full-width characters')
       end
