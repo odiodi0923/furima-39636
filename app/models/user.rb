57460bfcb28d91cbed19c18e6bdcac35143f2c53
odiodi0923 <141980992+odiodi0923@users.noreply.github.com>
2023-10-22 09:13:45 +0900

    商品詳細表示機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+  has_many :purchase_records
+
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: ' is invalid. Include both letters and numbers' }
 
