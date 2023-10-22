e4508905a420194dfc16706dabb3678db0e04b0f
odiodi0923 <141980992+odiodi0923@users.noreply.github.com>
2023-10-22 20:25:02 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :access_correct, only: [:edit]
 
   def index
     @items = Item.order('created_at DESC')
@@ -23,10 +24,36 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    # 以下、間違えて記述。購入機能実装の際に実装。
+    # if @item.purchase_record.present?
+    # redirect_to root_path
+    # end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:item_image, :item_name, :description, :category_id, :condition_id, :fee_option_id, :region_id,
                                  :delivery_d_id, :price).merge(user_id: current_user.id)
+  end
+
+  def access_correct
+    @item = Item.find(params[:id])
+
+    return if @item.user.id == current_user.id
+
+    redirect_to root_path
   end
 end
